3d3a56d561efd868d91f7dba768d374323186a4a
Nay Htun Linn <naytunlinn94@gmail.com>
2023-01-05 22:08:23 +0630

    init commit

---
+++ src/main/java/business/Mpt.java
@@ -0,0 +1,15 @@
+package business;
+
+import com.inter.sim;
+
+public class Mpt implements sim {
+
+    public void browse() {
+        System.out.println("Mpt Browse");
+    }
+
+    public void call() {
+        System.out.println("Mpt Call");
+
+    }
+}
---
+++ src/main/java/com/coder/Robot.java
@@ -0,0 +1,8 @@
+package com.coder;
+
+public class Robot {
+
+    public void job(){
+        System.out.println("Robot Start Walking");
+    }
+}
