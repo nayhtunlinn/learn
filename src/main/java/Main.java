195b0bd8887432cdc06af5df953d7178fb5a4d68
Nay Htun Linn <naytunlinn94@gmail.com>
2023-01-16 22:07:29 +0630

    Spaces are added to Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,19 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
+
+
+
+
+
+
+
+
+
+
+
+
+    
     public static void main(String[] args) {
         ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
 
